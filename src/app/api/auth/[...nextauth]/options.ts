72e124b091e7e5a6053ec4ffaa28d361f20019c8
Nahid <ghorbani.nca@gmail.com>
2023-07-31 01:48:01 +0330

    Configuring Provider in options

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,44 @@
 import type { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const options: NextAuthOptions = {
-  providers: [],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        username: {
+          label: "Username:",
+          type: "text",
+          placeholder: "enter-your-username",
+        },
+        password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "enter-your-password",
+        },
+      },
+      async authorize(credentials) {
+        //user data from api
+        const user = {
+          id: "42",
+          name: "Nahid",
+          password: "nextauth",
+        };
+
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
 };
